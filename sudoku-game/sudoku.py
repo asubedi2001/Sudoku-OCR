7734ce715756be12c6d3af86535487c98d9abbf2
Aakash Subedi <aakash.subedi20@gmail.com>
2024-10-26 21:03:32 -0400

    Implement solution_count(), which finds the total number of solutions a sudoku board has. Will help later determine if generated boards are malformed.

--- sudoku-game/sudoku.py
+++ sudoku-game/sudoku.py
@@ -53,29 +53,28 @@ class Sudoku:
         return False
         
     def solution_count(self):
-        # need main
-        solution_count = 0
+        num_solves = 0
         
         # find any empty cell as target to solve
         target_row, target_col = self.find_empty_cell()
 
-        # if all cells filled, only 1 solution
-        if not target_row:
+        # if all cells filled, solution found
+        if target_row is None:
             return 1 # puzzle solved already
         
         # recursive case, empty cells exist
         for target_val in range(1, 10):
             if self.validate_update(target_row, target_col, target_val):
                 self.matrix[target_row][target_col] = target_val
-                if self.backtrack_solve():
-                    solution_count += 1 # version of puzzle solved
-
-                # if no more valid updates exist,
+                
+                # aggregate solutions found
+                num_solves += self.solution_count()
+                
                 # erase this cell and try again with next target_val
                 self.matrix[target_row][target_col] = 0
         
         # return number of solutions found
-        return solution_count
+        return num_solves
         
     # TODO: Create function to generate a **uniquely** solvable sudoku board
     def generate_board(self):
@@ -84,9 +83,9 @@ class Sudoku:
         return
         
     def print_board(self):
-        print('\n')
         for row in range(len(self.matrix)):
             print(self.matrix[row])
+        print('\n')
 
 # random sudoku board someone was struggling with on reddit
 # https://www.reddit.com/r/sudoku/comments/1gc2oij/have_not_been_stuck_this_bad_in_a_long_time_what/
@@ -102,7 +101,28 @@ matrix = [[0,0,3, 0,4,0, 8,6,9],
           [0,0,2, 0,0,3, 6,7,5],
           [0,0,0, 4,0,6, 9,0,0]]
 
+# "Deadly Pattern" - 2 solutions
+# https://www.reddit.com/r/sudoku/comments/1c35yrk/sudoku_with_2_solutions/
+multi_solution_matrix = [[0,0,9, 4,1,5, 8,7,3],
+                         [5,8,1, 7,6,3, 9,2,4],
+                         [0,0,3, 9,2,8, 1,6,5],
+
+                         [0,0,4, 1,5,7, 2,3,8],
+                         [8,1,2, 3,9,6, 5,4,7],
+                         [3,5,7, 2,8,4, 6,9,1],
+
+                         [1,3,5, 6,7,2, 4,8,9],
+                         [0,0,6, 8,3,1, 7,5,2],
+                         [0,0,8, 5,4,9, 3,1,6]]
+
 sudoku = Sudoku(matrix)
+print("Board 1: ")
 sudoku.print_board()
 sudoku.backtrack_solve()
+print("Board 1 (Solved): ")
 sudoku.print_board()
+
+malformed = Sudoku(multi_solution_matrix)
+print("Board 2 (Malformed, 2 solutions): ")
+malformed.print_board()
+print(f'Number of calculated solutions: {malformed.solution_count()}') # prints 2, correct
